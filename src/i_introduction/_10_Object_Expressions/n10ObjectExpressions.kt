30a479728568200c7e6c1548cee938a3c635ed61
Przemysław Wojnowski <esperanto@cumego.com>
2018-03-08 09:44:51 +0100

    Solution to koan 10 - object expression.

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -18,6 +18,14 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, object : Comparator<Int?> {
+        override fun compare(a: Int?, b: Int?): Int {
+            if (a == null && b == null) return 0
+            if (a == null) return -1
+            if (b == null) return 1
+            return b.compareTo(a)
+        }
+
+    })
     return arrayList
 }
