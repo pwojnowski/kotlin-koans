c20256451248adc7f6be32bd1288c65a3509b830
Przemysław Wojnowski <esperanto@cumego.com>
2018-03-15 16:56:19 +0100

    Solution to koan 39 - forEachIndexed

--- src/v_builders/n39HtmlBuilders.kt
+++ src/v_builders/n39HtmlBuilders.kt
@@ -23,18 +23,29 @@ fun renderProductTable(): String {
     return html {
         table {
             tr {
-                td {
+                td (getTitleColor()) {
                     text("Product")
                 }
-                td {
+                td (getTitleColor()) {
                     text("Price")
                 }
-                td {
+                td (getTitleColor()) {
                     text("Popularity")
                 }
             }
-            val products = getProducts()
-            todoTask39()
+            getProducts().forEachIndexed { index, product ->
+                tr {
+                    td (getCellColor(index, 1)) {
+                        text(product.description)
+                    }
+                    td (getCellColor(index, 2)) {
+                        text(product.price)
+                    }
+                    td (getCellColor(index, 3)) {
+                        text(product.popularity)
+                    }
+                }
+            }
         }
     }.toString()
 }
